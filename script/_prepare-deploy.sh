9321b21aeac19bd0b64a1b61c89c1068a0161038
Pedro Trujillo <pedro@redmic.es>
2018-08-20 11:57:12 +0100

    Añade salida a la fase de preparación de deploy

--- script/_prepare-deploy.sh
+++ script/_prepare-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo -e "\n${INFO_COLOR}Preparing deploy configuration and resources ..${NULL_COLOR}\n"
+echo -e "\n${INFO_COLOR}Preparing deploy configuration and resources ..${NULL_COLOR}"
 
 # Se comprueba si se despliega desde dentro de 'deploy' o desde la raíz del proyecto.
 if [ -d "${DEPLOY_DIR_NAME}" ]
@@ -10,6 +10,8 @@ then
 else
 	deployFiles=${DEFAULT_DEPLOY_FILES}
 fi
+
+echo -e "\n${INFO_COLOR}Setting environment variables to local and remote environments ..${NULL_COLOR}"
 
 # Los argumentos pasados (opcionales) se tratan como variables de entorno.
 # Se prepara el fichero .env para usarlas en la máquina destino y se setean en este entorno también.
@@ -21,15 +23,24 @@ do
 done
 echo -e ${envDefs} >> .env
 
+echo -e "\n${INFO_COLOR}Checking deploy configuration in docker-compose files ..${NULL_COLOR}"
+echo -e "  ${INFO_COLOR}compose files [ ${DATA_COLOR}${COMPOSE_FILE}${INFO_COLOR} ]${NULL_COLOR}"
+
 # Antes de continuar, se comprueba que la configuración de despliegue sea válida.
 docker-compose config > /dev/null
 if [ "${?}" -ne "0" ]
 then
 	echo -e "${FAIL_COLOR}Invalid docker-compose configuration!${NULL_COLOR}"
 	exit 1
+else
+	echo -e "${PASS_COLOR}Valid docker-compose configuration!${NULL_COLOR}"
 fi
 
 DEPLOY_HOME="${DEPLOY_PATH}/docker/${STACK:-${SERVICE}}"
+
+echo -e "\n${INFO_COLOR}Sending deploy resources to remote ..${NULL_COLOR}"
+echo -e "  ${INFO_COLOR}deploy path [ ${DATA_COLOR}${DEPLOY_HOME}${INFO_COLOR} ]${NULL_COLOR}"
+echo -e "  ${INFO_COLOR}deploy files [ ${DATA_COLOR}${deployFiles}${INFO_COLOR} ]${NULL_COLOR}"
 
 # Se crea el directorio donde guardar los ficheros de despliegue del servicio.
 createDirCmd="mkdir -p ${DEPLOY_HOME}"
