5aa2184ec08f62aef73d4118ac35e5df4a469c27
Pedro Trujillo <pedro@redmic.es>
2018-09-06 12:28:42 +0100

    Corrige copia de ficheros de despliegue
    Simplifica el código de algunas condiciones y cambia la manera de
    copiar los recursos de despliegue al objetivo remoto. En lugar de usar
    el nombre del stack o service para nombrar a su directorio, genera un
    nombre aleatorio, para evitar que servicios pertenecientes al mismo
    stack compartan esa ruta. Además, al desplegar se elimina por completo
    el directorio.

--- script/_do-deploy.sh
+++ script/_do-deploy.sh
@@ -5,20 +5,19 @@ echo -e "\n${INFO_COLOR}Deploying at remote target ..${NULL_COLOR}\n"
 deployCmd="\
 	cd ${DEPLOY_HOME} && \
 	docker login -u ${REGISTRY_USER} -p ${CI_JOB_TOKEN} ${CI_REGISTRY} && \
-	docker stack ls > /dev/null 2> /dev/null ; \
-	if [ \"\${?}\" -ne \"0\" ] ; \
+	if docker stack ls > /dev/null 2> /dev/null ; \
 	then \
+		composeFileSplitted=\$(echo ${COMPOSE_FILE} | sed 's/:/ -c /g') && \
+		env -i \$(grep -v '^#\\| ' .env | xargs) \
+			docker stack deploy -c \${composeFileSplitted} --with-registry-auth ${STACK:-${SERVICE}} ; \
+	else \
 		docker-compose stop ${SERVICE} && \
 		docker-compose rm -f ${SERVICE} && \
 		docker-compose pull ${SERVICE} && \
-		docker-compose up -d ${SERVICE} && \
-		rm ${DEFAULT_DEPLOY_FILES} ; \
-	else \
-		composeFileSplitted=\$(echo ${COMPOSE_FILE} | sed 's/:/ -c /g') && \
-		env -i \$(grep -v '^#\\| ' .env | xargs) \
-			docker stack deploy -c \${composeFileSplitted} --with-registry-auth ${STACK:-${SERVICE}} && \
-		rm ${DEFAULT_DEPLOY_FILES} ; \
-	fi\
+		docker-compose up -d ${SERVICE} ; \
+	fi ; \
+	cd .. && \
+	rm -rf ${DEPLOY_HOME}\
 "
 
 ssh ${SSH_PARAMS} "${SSH_REMOTE}" "${deployCmd}"
