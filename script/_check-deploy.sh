388a13e0c845c654c7d53584b88b5f6fbeb64afe
Pedro Trujillo <pedro@redmic.es>
2019-02-04 09:47:20 +0000

    Escapa variables para reemplazar en remoto

--- script/_check-deploy.sh
+++ script/_check-deploy.sh
@@ -5,9 +5,9 @@ SERVICES_TO_CHECK="${SERVICES_TO_CHECK:-${STACK:-${SERVICE}}}"
 echo -e "\n${INFO_COLOR}Checking deployment of services [${DATA_COLOR} ${SERVICES_TO_CHECK} ${INFO_COLOR}] at ${DATA_COLOR}${remoteHost}${INFO_COLOR} ..${NULL_COLOR}"
 
 checkDeployCmd="\
-	for serviceToCheck in ${SERVICES_TO_CHECK} ; \
+	for serviceToCheck in \${SERVICES_TO_CHECK} ; \
 	do \
-		echo -e \"\\n${INFO_COLOR}Checking deployment of service ${DATA_COLOR}${serviceToCheck}${INFO_COLOR} ..${NULL_COLOR}\" ; \
+		echo -e \"\\n${INFO_COLOR}Checking deployment of service ${DATA_COLOR}\${serviceToCheck}${INFO_COLOR} ..${NULL_COLOR}\" ; \
 		echo -e \"  ${INFO_COLOR}retries ${DATA_COLOR}${STATUS_CHECK_RETRIES}${INFO_COLOR}, interval ${DATA_COLOR}${STATUS_CHECK_INTERVAL}${INFO_COLOR}s, hits ${DATA_COLOR}${STATUS_CHECK_MIN_HITS}${NULL_COLOR}\\n\" ; \
 
 		hits=0 && \
