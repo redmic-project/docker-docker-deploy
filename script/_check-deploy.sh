72e12ac157da271bb536f77dd19b29f30e44df5c
Pedro Trujillo <pedro@redmic.es>
2019-02-05 09:12:01 +0000

    Revisa y corrige lógica de check deploy

--- script/_check-deploy.sh
+++ script/_check-deploy.sh
@@ -24,25 +24,34 @@ checkDeployCmd="\
 					echo -e \"${FAIL_COLOR}Found ${DATA_COLOR}\${serviceCount}${FAIL_COLOR} services filtering by name ${DATA_COLOR}\${serviceToCheck}${FAIL_COLOR},${NULL_COLOR}\" ; \
 					echo -e \"  ${FAIL_COLOR}will check only the service exactly named ${DATA_COLOR}\${runningServiceName}${NULL_COLOR}\\n\" ; \
 				fi ; \
-				runningServiceCount=\$(echo \"\${serviceToCheckReplication}\" | ${GREP_BIN} -cE '([0-9]+)\/\1') ; \
-				serviceIsRunning=\"[ \${runningServiceCount} -eq 1 ]\" ; \
-				if ! \${serviceIsRunning} ; \
+				if [ \${serviceCount} -eq 0 ] ; \
 				then \
-					serviceToCheckDesiredReplicas=\$(echo \${serviceToCheckReplication} | cut -d '/' -f 2) ; \
-					completedTaskCount=0 ; \
-					for j in \$(seq 1 \${serviceToCheckDesiredReplicas}) ; \
-					do \
-						replicaStoppedTaskState=\$(docker service ps --format '{{.CurrentState}}' \
-							-f 'desired-state=shutdown' -f \"name=\${runningServiceName}.\${j}\" \
-							\${runningServiceName} | head -1) ; \
-						if echo \"\${replicaStoppedTaskState}\" | ${GREP_BIN} 'Complete' > /dev/null 2> /dev/null ; \
-						then \
-							completedTaskCount=\$((\${completedTaskCount} + 1)) ; \
-						fi ; \
-					done ; \
-					if [ \${completedTaskCount} -eq \${serviceToCheckDesiredReplicas} ] ; \
+					if [ \${i} -eq 1 ] ; \
 					then \
-						serviceIsRunning=true ; \
+						echo -e \"${FAIL_COLOR}Cannot find running service by name ${DATA_COLOR}\${serviceToCheck}${NULL_COLOR}\\n\" ; \
+					fi ; \
+					serviceIsRunning=\"[ 0 -ne 0 ]\" ; \
+				else \
+					runningServiceCount=\$(echo \"\${serviceToCheckReplication}\" | ${GREP_BIN} -cE '([0-9]+)\/\1') ; \
+					serviceIsRunning=\"[ \${runningServiceCount} -eq 1 ]\" ; \
+					if ! \${serviceIsRunning} ; \
+					then \
+						serviceToCheckDesiredReplicas=\$(echo \${serviceToCheckReplication} | cut -d '/' -f 2) ; \
+						completedTaskCount=0 ; \
+						for j in \$(seq 1 \${serviceToCheckDesiredReplicas}) ; \
+						do \
+							replicaStoppedTaskState=\$(docker service ps --format '{{.CurrentState}}' \
+								-f 'desired-state=shutdown' -f \"name=\${runningServiceName}.\${j}\" \
+								\${runningServiceName} | head -1) ; \
+							if echo \"\${replicaStoppedTaskState}\" | ${GREP_BIN} 'Complete' > /dev/null 2> /dev/null ; \
+							then \
+								completedTaskCount=\$((\${completedTaskCount} + 1)) ; \
+							fi ; \
+						done ; \
+						if [ \${completedTaskCount} -eq \${serviceToCheckDesiredReplicas} ] ; \
+						then \
+							serviceIsRunning=true ; \
+						fi ; \
 					fi ; \
 				fi ; \
 				statusCheckCmd=\${serviceIsRunning} ; \
@@ -63,19 +72,22 @@ checkDeployCmd="\
 				hits=\$((\${hits} + 1)) && \
 				if [ \"\${hits}\" -eq \"${STATUS_CHECK_MIN_HITS}\" ] ; \
 				then \
-					echo -e \"${PASS_COLOR}Service ${DATA_COLOR}\${serviceToCheck}${PASS_COLOR} is running!${NULL_COLOR}\" && \
+					echo -e \"\\n${PASS_COLOR}Service ${DATA_COLOR}\${serviceToCheck}${PASS_COLOR} is running!${NULL_COLOR}\" && \
 					echo -e \"  got ${PASS_COLOR}\${hits}/${STATUS_CHECK_MIN_HITS}${NULL_COLOR} status hits\" && \
 					success=\"\${success} 1\" ; \
 					break ; \
 				fi ; \
 			else \
 				echo -e \"${FAIL_COLOR}[FAIL]${NULL_COLOR}\" ; \
+				if [ \"\${i}\" -eq \"${STATUS_CHECK_RETRIES}\" ] ; \
+				then \
+					echo -e \"\\n${FAIL_COLOR}Service ${DATA_COLOR}\${serviceToCheck}${FAIL_COLOR} is not running!${NULL_COLOR}\" && \
+					echo -e \"  got ${FAIL_COLOR}\${hits}/${STATUS_CHECK_MIN_HITS}${NULL_COLOR} status hits\" && \
+					success=\"\${success} 0\" ; \
+				fi ; \
 			fi ; \
 			sleep ${STATUS_CHECK_INTERVAL} ; \
 		done ; \
-		echo -e \"${FAIL_COLOR}Service ${DATA_COLOR}\${serviceToCheck}${FAIL_COLOR} is not running!${NULL_COLOR}\" && \
-		echo -e \"  got ${FAIL_COLOR}\${hits}/${STATUS_CHECK_MIN_HITS}${NULL_COLOR} status hits\" && \
-		success=\"\${success} 0\" ; \
 	done ; \
 	for serviceSuccess in \${success[*]} ; \
 	do \
@@ -87,9 +99,9 @@ checkDeployCmd="\
 
 if ssh ${SSH_PARAMS} "${SSH_REMOTE}" "${checkDeployCmd}"
 then
-	echo -e "${PASS_COLOR}All services seems ok!${NULL_COLOR}"
+	echo -e "\\n${PASS_COLOR}All services seems ok!${NULL_COLOR}"
 else
-	echo -e "${FAIL_COLOR}One or more services seems failed!${NULL_COLOR}"
+	echo -e "\\n${FAIL_COLOR}One or more services seems failed!${NULL_COLOR}"
 	ssh ${SSH_PARAMS} -q -O exit "${SSH_REMOTE}"
 	exit 1
 fi
